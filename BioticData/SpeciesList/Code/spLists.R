d3e7f22c8e940dd33705bc3f1decb91693845d83
David Beauchesne <david.beauchesne@uqar.ca>
2020-01-21 15:20:00 -0500

    update species list

--- BioticData/SpeciesList/Code/spLists.R
+++ BioticData/SpeciesList/Code/spLists.R
@@ -1,19 +1,16 @@
 load('./BioticData/Combine_RelevePluriSp_MPO/Data/CombinePluriSP.RData')
 load('./BioticData/ZIF-Fisheries-2010-2015/Data/Biotic/zifSp.RData')
 load('./BioticData/ObsMer_1999-2015_MPO/Data/Biotic/SeaObserverSP.RData')
+load('BioticData/Peche_sentinelle_2011-2015/Data/Biotic/SentinelleFixedSP.RData')
+load('BioticData/Peche_sentinelle_2011-2015/Data/Biotic/SentinelleMobileSP.RData')
 spList$CountRec <- spList$combRec
 spList <- spList[, c('species','CountRec')]
 
 library(magrittr)
 library(tidyverse)
-sp <- rbind(spList, zifSp, obsSp) %>%
+sp <- rbind(spList, zifSp, obsSp, sentMobSp, sentSp) %>%
       group_by(species) %>%
       summarise(Count = sum(CountRec)) %>%
       as.data.frame()
 
 save(sp, file = './BioticData/SpeciesList/Data/SpeciesList.RData')
-
-
-
-load('BioticData/Peche_sentinelle_2011-2015/Data/Biotic/SentinelleFixedSP.RData')
-load('BioticData/Peche_sentinelle_2011-2015/Data/Biotic/SentinelleMobileSP.RData')
